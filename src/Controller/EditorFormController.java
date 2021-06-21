6d6da0706909a19407870e617172039acfcce90f
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-06-21 23:15:43 +0530

    Initial commit

---
+++ src/Controller/EditorFormController.java
@@ -0,0 +1,48 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class EditorFormController {
+
+    public AnchorPane pneFind;
+    public TextField txtFind;
+    public TextArea txtEditor;
+    public AnchorPane pneReplace;
+    public TextField txtReplace;
+
+    public void mnuitemnew_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuItemCut_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuItemCopy_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuItemPaste_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuItemFind_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuItemReplace_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuItemSelectAll_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnNext_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPrevious_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnReplace_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnReplaceAll_OnAction(ActionEvent actionEvent) {
+    }
+}
